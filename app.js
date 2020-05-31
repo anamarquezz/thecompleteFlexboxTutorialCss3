d3a580b8855712335f8d03a39e960df60d23fa71
AnaMarquez <anamarquez.edu@hotmail.com>
2020-05-31 16:02:55 -0700

    align items

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 
-document.querySelector('.divclick').addEventListener('click',function(e){
-    window.location.href = 'FlexContainers/' + e.currentTarget.classList[1] + '.html';
- });
+
+ var elements = document.getElementsByClassName("divclick");
+
+
+ for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click',function(e){
+        window.location.href = 'FlexContainers/' + e.currentTarget.classList[1] + '.html';
+  })
+ }
