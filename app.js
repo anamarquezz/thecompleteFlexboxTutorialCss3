1ed9a605d4e6a723c072e7fb1d198e7277a0b51b
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-02 20:10:27 -0700

    order

--- app.js
+++ app.js
@@ -8,3 +8,13 @@
         window.location.href = 'FlexContainers/' + e.currentTarget.classList[1] + '.html';
   })
  }
+
+
+ 
+ var elements = document.getElementsByClassName("c2");
+ for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click',function(e){
+        window.location.href = 'FlexItems/' + e.currentTarget.classList[2] + '.html';
+  })
+ }
+
