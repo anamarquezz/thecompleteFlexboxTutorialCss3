a444a4e28d2b3ebb5bbfe0d2588a2d635f4bb068
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-02 23:44:31 -0700

    real vertical centering

--- app.js
+++ app.js
@@ -22,6 +22,7 @@
  var elements = document.getElementsByClassName("c3");
  for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click',function(e){
+        console.log(e.currentTarget.classList[2])
         window.location.href = 'FlexboxAwesomeness/' + e.currentTarget.classList[2] + '.html';
   })
  }
